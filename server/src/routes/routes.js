095d90e5d987ea324d8c2a5ee81f4ed2b252e95c
Zornitsa Stoyanova <zorito_999@abv.bg>
2025-05-11 14:58:49 +0300

    chore: separate ci-cd jobs for pr and main

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -3,11 +3,15 @@ const router = express.Router();
 
 const usersRoute = require('./user.route');
 const aiRoutes = require('./ai.route')
+const healthRoute = require('./health.route');
 
 // Add user routing
 router.use('/users', usersRoute);
 
 // Add ai routing
 router.use('/ai', aiRoutes);
+
+// Add health routing for CI
+router.use('/', healthRoute);
 
 module.exports = router
